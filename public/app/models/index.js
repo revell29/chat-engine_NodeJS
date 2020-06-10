655c105b9fa74925a83233a1925091af80891940
revell29 <diraapsya0@gmail.com>
2020-06-10 20:20:40 +0700

    change file name in server

--- public/app/models/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _Message = _interopRequireDefault(require("./server/Message"));
-
-var _User = _interopRequireDefault(require("./server/User"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
